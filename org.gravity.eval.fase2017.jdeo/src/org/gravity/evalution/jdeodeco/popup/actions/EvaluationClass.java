bb336e674c339ba7c3ee66835b2e118c1a2b56b0
Sven Peldszus <speldszus@uni-koblenz.de>
2016-10-20 18:24:48 +0200

    Introduced measurements of multiple projects

--- org.gravity.eval.fase2017.jdeo/src/org/gravity/evalution/jdeodeco/popup/actions/EvaluationClass.java
+++ org.gravity.eval.fase2017.jdeo/src/org/gravity/evalution/jdeodeco/popup/actions/EvaluationClass.java
@@ -1,9 +1,21 @@
 package org.gravity.evalution.jdeodeco.popup.actions;
 
-import java.lang.reflect.Field;
+import java.io.BufferedWriter;
+import java.io.ByteArrayInputStream;
+import java.io.FileWriter;
 import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
+import javax.print.CancelablePrintJob;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IFolder;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.jdt.core.ICompilationUnit;
 import org.eclipse.jdt.core.IJavaProject;
 import org.eclipse.jface.action.IAction;
@@ -16,9 +28,9 @@ import org.eclipse.ui.ISelectionListener;
 import org.eclipse.ui.IWorkbenchPart;
 import org.eclipse.ui.PlatformUI;
 
-import gr.uom.java.ast.CompilationUnitCache;
+import gr.uom.java.distance.Entity;
 import gr.uom.java.distance.ExtractClassCandidateGroup;
-import gr.uom.java.jdeodorant.refactoring.views.GodClass;
+import gr.uom.java.distance.ExtractClassCandidateRefactoring;
 
 public class EvaluationClass implements IObjectActionDelegate {
 
@@ -43,21 +55,21 @@ public class EvaluationClass implements IObjectActionDelegate {
 	 * Gets selected project from workspace
 	 * @return
 	 */
-	private IJavaProject getSelectedProject() {
-			IJavaProject javaProject = null;
+	private Set<IJavaProject> getSelectedProject() {
+		Set<IJavaProject> javaProject = null;
 			
-			ISelection selection =
-					PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().getSelection();
+		ISelection selection = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().getSelection();
 
-			if (selection instanceof IStructuredSelection) {
-				IStructuredSelection structuredSelection = (IStructuredSelection)selection;
-				Object element = structuredSelection.getFirstElement();
-				
+		if (selection instanceof IStructuredSelection) {
+			javaProject = new HashSet<>();
+			IStructuredSelection structuredSelection = (IStructuredSelection)selection;
+			for(Object element: ((IStructuredSelection) selection).toArray()){
 				if(element instanceof IJavaProject) {
-					javaProject = (IJavaProject)element;
+					javaProject.add((IJavaProject)element);
 				}
 			}
-	return javaProject;
+		}
+		return javaProject;
 	}
 
 
@@ -67,79 +79,81 @@ public class EvaluationClass implements IObjectActionDelegate {
 	 */
 	public void run(IAction action) {
 	 
-		GodClass viewGodClass = new GodClass();
-		
-		try {
+		// Get the currently selected projects
+		for(IJavaProject sProject : getSelectedProject()){
+			try {
+				String projName = sProject.getProject().getName();
 			
-			// Get the currently selected project
-			IJavaProject sProject = getSelectedProject();
-			String projName = sProject.toString();
-			
-			// Set up time
-			System.out.println("Start time measurement for project " +	projName + ".");
-			long timePrev = System.currentTimeMillis();
-			
-			// Set selectedProject and activeProject in the GodClass by Reflection
-			Field selProject = viewGodClass.getClass().getDeclaredField("selectedProject");
-			selProject.setAccessible(true);
-			selProject.set(viewGodClass, sProject);
-			
-			Field actProject = viewGodClass.getClass().getDeclaredField("activeProject");
-			actProject.setAccessible(true);
-			actProject.set(viewGodClass, sProject);
-			
-			
-			
-			
-			// Clear Cache
-			CompilationUnitCache.getInstance().clearCache();
-			
-			
-			// Probably not needed
-			ExtractClassCandidateGroup[] candidateRefactoringTable;
-			
-			
-			// Execute the search for code smells and refactoring options by getTable() via reflection
-			Method getTable = viewGodClass.getClass().getDeclaredMethod("getTable");
-			getTable.setAccessible(true);
-			candidateRefactoringTable = (ExtractClassCandidateGroup[]) getTable.invoke(viewGodClass);
-			
-			
-			// End time measurement
-			long timeEnd = System.currentTimeMillis();
-			System.out.println("Time taken for JDeodorant check for project " +	projName + " : " + (timeEnd - timePrev) + "ms");
-			System.out.println("");
-			
-			
-		} catch (SecurityException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (NoSuchFieldException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IllegalArgumentException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IllegalAccessException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (NoSuchMethodException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (InvocationTargetException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		
-		
-		
+				// Set up time
+				System.out.println("Start time measurement for project " +	projName + ".");
+				long timePrev = System.currentTimeMillis();
+				
+				ExtractClassCandidateGroup[] candidateRefactoringTable = EvaluateGodClass.detect(sProject);
+				
+				// End time measurement
+				long timeEnd = System.currentTimeMillis();
+				String string = "Time taken for JDeodorant check for project " +	projName + " : " + (timeEnd - timePrev) + "ms";
+				System.out.println(string);
+				System.out.println("");
 
+				List<Entity> extract = null;
+				if(candidateRefactoringTable!=null){
+				double min = Double.MAX_VALUE;
+				for(ExtractClassCandidateGroup eccg : candidateRefactoringTable){
+					ArrayList<ExtractClassCandidateRefactoring> candidates = eccg.getCandidates();
+					for(ExtractClassCandidateRefactoring c : candidates){
+						if(min > c.getEntityPlacement()){
+							min = c.getEntityPlacement();
+							extract = c.getExtractedEntities();
+						}
+					}
+				}
+				}
+				IFolder gravity = sProject.getProject().getFolder("gravity");
+				if(!gravity.exists()){
+					try {
+						gravity.create(true, true, new NullProgressMonitor());
+					} catch (CoreException e) {
+						e.printStackTrace();
+					}
+				}
+				IFile jdeo = gravity.getFile("jdeo"+System.currentTimeMillis()+".txt");
+				try {
+					if(extract!=null){
+						string += "\n Extract "+extract.size()+" entities: "+extract;
+					}
+					else{
+						string += "\n Extract is null";
+					}
+					jdeo.create(new ByteArrayInputStream(string.getBytes()), true, new NullProgressMonitor());
+				} catch (CoreException e) {
+					e.printStackTrace();
+				}
+				
+			} catch (SecurityException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (NoSuchFieldException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IllegalArgumentException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IllegalAccessException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (NoSuchMethodException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (InvocationTargetException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		
-		
+		}		
 		
 	}
-
+	
 	/**
 	 * @see IActionDelegate#selectionChanged(IAction, ISelection)
 	 */
